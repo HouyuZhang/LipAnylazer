608ef45b3bde6c2d7a6a7be44637fe4cf1d8cc88
Houyu Zhang <hughiez047@gmail.com>
2021-12-04 17:22:03 +0800

    update

--- Lipidomics1Renal.R
+++ Lipidomics1Renal.R
@@ -28,7 +28,7 @@ devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes
 # ==============================================================================
 # 1. Tidy data and run functions
 # ==============================================================================
-source("../210923_Lipidomics/LipidomicsAnlysis.R")
+source("D:/OneDrive/Github_respiratories/Lipidomics_analysis/LipidomicsAnlysis.R")
 MatchLipidMAPS(MeasurementsFile_standardized = "Lipidomics_5depots_rawdata.csv", 
                LipidMAPSDB = "lipid_database.csv",
                IdentificationFile = "Lipidomics_5depots_Identificaitons.csv", 
@@ -60,9 +60,16 @@ MatchLipidMAPS(MeasurementsFile_standardized = "standardized_Measurements_RT_TN.
 Format_mummichog_input(MeasurementsFile_standardized = "standardized_Measurements_RT_TN.csv", 
                        OutputPairWise = T, SelectedSample = "")
 
-mummichog_Functional_analysis(pktablePath = "standardized_Measurements_RT_TN_mummichogInput_tn_renal_ma+rt_renal_ma.csv", 
-                              PvalueThreshold = 0.5, Species = "mmu", rowNormMet = "NULL", EnrichType = "KEGG",
-                              SamplesDel = "rt_renal_ma_3")
+mummichog_Functional_analysis(pktablePath = "functional_hfdcd_ing_ma.csv", 
+                              PvalueThreshold = 1, Species = "mmu", rowNormMet = "NULL", EnrichType = "MainClass",
+                              SamplesDel = "hfd_ing_ma_3")
+
+mummichog_Functional_analysis(pktablePath = "functional_hfdcd_ing_ma.csv",
+                              PvalueThreshold = 0.15,
+                              Species = "mmu",
+                              rowNormMet = "NULL",
+                              EnrichType = "KEGG",
+                              SamplesDel = "hfd_ing_ma_3")
 
 Refine_MZ_Identifications(MeasurementsFile_standardized_matched = "standardized_Measurements_RT_TN_matched.csv",
                           RefinePlan = "PlanC")
@@ -70,5 +77,43 @@ Refine_MZ_Identifications(MeasurementsFile_standardized_matched = "standardized_
 Analyze_Lipids(MeasurementsFile_standardized_matched_refined = "standardized_Measurements_RT_TN_matched_refined_PlanC.csv",
                OutputPairWise = T, KeywordSelected = "rt", run_RunMetaboAnalystR = T)
 
+# ==============================================================================
+# 1. Tidy data and run functions
+# ==============================================================================
+Level = c("main_class","sub_class","abbrev")[3]
+
+  read_csv("Lipidomics_5depots_rawdata_matched_refined_PlanC.csv") %>%
+  select(c(compound:adi_epi_3),starts_with("adi_peri")) %>%
+  mutate_at(vars(starts_with("adi")), funs(.*100000/sum(.))) %>%
+  filter(str_detect(main_class,"Ceramide")) %>%
+  filter(str_detect(sub_class,"acylsphingosines")) %>%
+  select_at(vars(Level, starts_with("adi"))) %>%
+  group_by(!!as.name(Level)) %>% 
+  mutate_if(is.character,as.numeric) %>% 
+  summarise_all(c("sum")) %>%
+  filter(!!as.name(Level) != "-") %>% 
+  filter(rowSums(across(where(is.numeric))) != 0) %>%
+  reshape2::melt(c(Level)) %>% 
+  mutate(group = gsub("_[0-9]","",variable)) %>%
+  ggplot(aes(x = group, y = value, fill = group)) + 
+    geom_boxplot(notch=F, size=0.4) +
+    geom_jitter(width = 0.1, alpha = 0.5,color = "black") + 
+    ggpubr::stat_compare_means(method = "t.test", paired = F, 
+                               comparisons = list(c("adi_epi", "adi_peri"))) +
+    scale_fill_manual(values = c("#66A61E","#D95F02")) +
+    facet_wrap(~abbrev, ncol =6, scales = "free") +
+    labs(x = "", y = "Relative lipid signal") +
+    theme_bw() +
+    theme(
+      axis.title.x = element_text(color="black", size=14, face="bold"),
+      # axis.text.x = element_text(color="black", size=11, face="bold",angle = 40, hjust = 1),
+      axis.text.x = element_blank(),
+      axis.title.y = element_text(color="black", size=14, face="bold"),
+      axis.text.y = element_text(color="black", size=11, face="bold"),
+      legend.title = element_blank(),
+      legend.text = element_text(color="black", size=14, face="bold"),
+      
+    )
+ggsave("abbrev_22.pdf",width = 10, height = 6)
 
 
---
+++ Lipidomics2Lab.R
@@ -0,0 +1,66 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("D:/OneDrive/Github_respiratories/Lipidomics_analysis/LipidomicsAnlysis.R")
+
+# ==============================================================================
+# 1. iWAT differentiation
+# ==============================================================================
+setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/2_iWAT_differentiation/")
+Format_mummichog_input(MeasurementsFile_standardized = "iWATdiff_standardized_Measurements.csv", OutputPairWise = T)
+
+mummichog_Functional_analysis(pktablePath = "iWATdiff_standardized_Measurements_mummichogInput_ing_cell+ing_ma.csv", 
+                              Species = "mmu", rowNormMet = "NULL",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iWATdiff_standardized_Measurements_mummichogInput_ing_svf+ing_cell.csv", 
+                               Species = "mmu", rowNormMet = "NULL",
+                               PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iWATdiff_standardized_Measurements_mummichogInput_ing_svf+ing_ma.csv", 
+                               Species = "mmu", rowNormMet = "NULL",
+                               PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iWATdiff_standardized_Measurements_mummichogInput_ing.csv", 
+                              Species = "mmu", rowNormMet = "NULL",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+                              )
+Replot_mummichog(mummichog_PEpath = "./")
+
+# ==============================================================================
+# 2_iBAT_differentiation_noIS
+# ==============================================================================
+setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/2_iBAT_differentiation_noIS/")
+Format_mummichog_input(MeasurementsFile_standardized = "iBATdiff_Measurements.csv", OutputPairWise = T)
+
+mummichog_Functional_analysis(pktablePath = "iBATdiff_Measurements_mummichogInput_ibat_cell+ibat_ma.csv", 
+                              Species = "mmu", rowNormMet = "SumNorm",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iBATdiff_Measurements_mummichogInput_ibat_svf+ibat_cell.csv", 
+                              Species = "mmu", rowNormMet = "SumNorm",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iBATdiff_Measurements_mummichogInput_ibat_svf+ibat_ma.csv", 
+                              Species = "mmu", rowNormMet = "SumNorm",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+mummichog_Functional_analysis(pktablePath = "iBATdiff_Measurements_mummichogInput.csv", 
+                              Species = "mmu", rowNormMet = "SumNorm",
+                              PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
+)
+setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/")
+Replot_mummichog(mummichog_PEpath = "./")
+
+# ==============================================================================
+# 2. OB/Lean moneky
+# ==============================================================================
+setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/3_OBLean_Monkey/")
+Format_mummichog_input(MeasurementsFile_standardized = "OBLean_Monkey_standardized_Measurements.csv", OutputPairWise = F)
+
+mummichog_Functional_analysis(pktablePath = "OBLean_Monkey_standardized_Measurements_mummichogInput.csv", 
+                              Species = "hsa", rowNormMet = "NULL",
+                              PvalueThreshold = 0.4, EnrichType = c("KEGG","MainClass")
+)
+Replot_mummichog(mummichog_PEpath = "./")
+
+
+
