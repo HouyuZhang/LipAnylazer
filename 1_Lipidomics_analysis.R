fae54304b9f8b4c890ae2302df86183cf24d20cf
Houyu Zhang <houyuzhang@stu.pku.edu.cn>
2023-11-22 21:14:45 +0800

    update

--- 1_Lipidomics_analysis.R
+++ 1_Lipidomics_analysis.R
@@ -26,7 +26,7 @@ mummichog_Functional_analysis(pktablePath = "iWATdiff_standardized_Measurements_
 Replot_mummichog(mummichog_PEpath = "./")
 
 # ==============================================================================
-# 2_iBAT_differentiation_noIS
+# 2_iBAT_differentiation
 # ==============================================================================
 setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/2_iBAT_differentiation_noIS/")
 Format_mummichog_input(MeasurementsFile_standardized = "iBATdiff_Measurements.csv", OutputPairWise = T)
@@ -49,39 +49,3 @@ mummichog_Functional_analysis(pktablePath = "iBATdiff_Measurements_mummichogInpu
 )
 setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/")
 Replot_mummichog(mummichog_PEpath = "./")
-
-# ==============================================================================
-# 3. OB/Lean moneky
-# ==============================================================================
-setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/3_OBLean_Monkey/")
-Format_mummichog_input(MeasurementsFile_standardized = "OBLean_Monkey_standardized_Measurements.csv", OutputPairWise = F)
-
-mummichog_Functional_analysis(pktablePath = "OBLean_Monkey_standardized_Measurements_mummichogInput.csv", 
-                              Species = "hsa", rowNormMet = "NULL",
-                              PvalueThreshold = 0.4, EnrichType = c("KEGG","MainClass")
-)
-Replot_mummichog(mummichog_PEpath = "./")
-
-# ==============================================================================
-# 4. CE/RT moneky
-# ==============================================================================
-setwd("D:/OneDrive/4_PKU-CIBR/1_Rotation_CarlosLab/1_Lipidomics2Lab/1_CE_RT_noIS/")
-Format_mummichog_input(MeasurementsFile_standardized = "CE_RT_Measurements2.csv", OutputPairWise = T)
-
-mummichog_Functional_analysis(pktablePath = "CE_RT_Measurements2_mummichogInput_ma_ce_ibat_exp1+ma_rt_ibat_exp1.csv", 
-                              Species = "mmu", rowNormMet = "SumNorm", PvalueThreshold = 0.05, EnrichType = c("KEGG","MainClass")
-)
-mummichog_Functional_analysis(pktablePath = "CE_RT_Measurements2_mummichogInput_svf_ce_ibat_exp1+svf_rt_ibat_exp1.csv", 
-                               Species = "mmu", rowNormMet = "SumNorm", PvalueThreshold = 1, EnrichType = c("KEGG","MainClass")
-)
-
-mummichog_Functional_analysis(pktablePath = "CE_RT_Measurements2_mummichogInput_ma_ce_ibat_exp2+ma_rt_ibat_exp2.csv", 
-                              Species = "mmu", rowNormMet = "SumNorm", PvalueThreshold = 0.4, EnrichType = c("KEGG","MainClass")
-)
-mummichog_Functional_analysis(pktablePath = "CE_RT_Measurements2_mummichogInput_svf_ce_ibat_exp2+svf_rt_ibat_exp2.csv", 
-                              Species = "mmu", rowNormMet = "SumNorm", PvalueThreshold = 0.1, EnrichType = c("KEGG","MainClass")
-)
-
-
-Replot_mummichog(mummichog_PEpath = "../")
-
--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # :hammer_and_wrench:A unified pipeline for modular lipidomics analysis
-**:writing_hand:Author**: Houyu Zhang  **:email:Email**: Hughiez047@gmail.com
+**:writing_hand:Author**: Houyu Zhang  **:email:Email**: houyuzhang@stu.pku.edu.cn
 
 Copyright (c) 2021 CarlosLab@PKU. All rights reserved.
 
 ## Introduction
 
-The lipidomics technical has broaden our views on lipid metabolism from a systematical level. However, due to the intractable lipid annotation problem, current available analytic methods are designed specifically according to custom protocols (like *LC-MS based* or *shot-gun based*), thus can not be directly transplanted among laboratories. 
+The lipidomics technical has broadened our views on lipid metabolism from a systematical level. However, due to the intractable lipid annotation problem, current available analytic methods are designed specifically according to custom protocols (like *LC-MS based* or *shot-gun based*), thus can not be directly transplanted among laboratories. 
 
-In Carlos lab, we used the LC-MS based protocol to investigate lipid composition dynamics under different physiologic conditions. Thus we developed this pipeline for internal use on daily lipidomics data processing, it starts from raw identified compound information for standardization and normalization, then regular or custom analysis was conducted based on internal R functions.
+We used the LC-MS based protocol to investigate lipid composition dynamics under different physiologic conditions. Thus we developed this pipeline for internal use in daily lipidomics data processing, it starts from raw identified compound information for standardization and normalization, and then regular or custom analysis was conducted based on internal R functions.
 
-This pipeline was written in R language using publishable grammar with well-documented function modules, and will be released as standard R package later...
+This pipeline was written in R language using publishable grammar with well-documented function modules and will be released as a standard R package later.
+
+### 📁Scripts
+
+- R codes for all analysis were listed.
 
